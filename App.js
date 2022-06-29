8490f6c93524a004d65fde3d2743eb80432c099c
kevin siregar <kevinsiregar22>
2022-06-29 13:31:05 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+  return (
+    <View>
+      <Text>todo app</Text>
+    </View>
+  );
+};
+
+export default App;
